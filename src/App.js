d0c1e3552bf553d711d0ff2c03710f639ddf1cae
Dariusz Okoński <darek200180@gmail.com>
2024-03-15 17:23:57 +0100

    add json-server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useEffect, useReducer } from 'react';
 import Header from './Header';
 import Main from './Main';
 
+const initialState = {
+  questions: [],
+  // loading, error, ready, active, finished
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  return state;
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error('Error'));
+  }, []);
+
   return (
     <div className='app'>
       <Header />
