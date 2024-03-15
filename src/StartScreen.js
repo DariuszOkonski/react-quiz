ed83655a219a99b7c450d7293739a0e720482fad
Dariusz Okoński <darek200180@gmail.com>
2024-03-15 17:52:59 +0100

    add Question component

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-function StartScreen({ numQuestions }) {
+function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className='start'>
       <h2>Welcome to The React Quiz!</h2>
       <h3>{numQuestions} question to test your React mastery</h3>
-      <button className='btn btn-ui'>Let's start</button>
+      <button
+        onClick={() => dispatch({ type: 'start' })}
+        className='btn btn-ui'
+      >
+        Let's start
+      </button>
     </div>
   );
 }
